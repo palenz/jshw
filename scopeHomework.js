d48083a8831c264526a8fcdf4ee1a4bd61b106d7
Juan <jpalenzuela@outlook.com>
2021-01-06 09:17:56 +0000

    initial commit

---
+++ scopeHomework.js
@@ -0,0 +1,46 @@
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+// The murderer is Scarlet, as described in the scenario object.
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+// Murderer is Plum. Even though Peacock is in an universal variable,
+// Plum is in a constant variable that can't be changed.
+
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+
+// The first murderer will be Plum and then this will be changed to Peacock
